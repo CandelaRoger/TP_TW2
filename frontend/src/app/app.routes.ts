c575f961618402c699ccec21a132b0a91c53fe28
Victoria Diaz <victoria.diaz.98.vd@gmail.com>
2026-06-27 03:07:29 -0300

    subo hmtl de login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,15 +2,15 @@ import { Routes } from '@angular/router';
 import { CatalogoComponent } from './components/catalogo/catalogo';
 import { CarritoComponent } from './components/carrito/carrito';
 import { RegistroComponent } from './components/registro/registro';
-import { Landing} from './components/landing/landing';
-import { Login} from './components/login/login';
+import { Landing } from './components/landing/landing';
+import { Login } from './components/login/login';
 
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'landing', pathMatch: 'full' }, 
+  { path: '', redirectTo: 'landing', pathMatch: 'full' },
   { path: 'productos', component: CatalogoComponent },
   { path: 'carrito', component: CarritoComponent },
   { path: 'landing', component: Landing },
-  { path: 'login', component: Login } ,
-  { path: 'registro', component: RegistroComponent}
+  { path: 'login', component: Login },
+  { path: 'registro', component: RegistroComponent }
 ];
--- frontend/src/app/components/login/login.ts
+++ frontend/src/app/components/login/login.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrl: './login.css'
 })
-export class Login {}
+export class Login {
+
+  usuario = {
+    email: '',
+    password: ''
+  };
+
+  mensajeError = '';
+
+  onSubmit() {
+    console.log('Login con:', this.usuario);
+    // Acá después llamás al servicio que hace el POST al backend
+  }
+}
